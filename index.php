3adcc91a9a8653c5e0758a8fc66fdfb2bc7468cd
luisjin9 <62250044+luisjin9@users.noreply.github.com>
2022-06-02 18:41:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NORMAGRUP</title>
+</head>
+<body>
+    <form action="validar.php" method="post">
+    <h1>Calibraciones Normagrup</h1>
+    <p>Usuario <input type="text" placeholder="ingrese su nombre" name= "usuario"></p>
+    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name= "contraseña"></p>
+    <input type="submit" value= "Cargar">
+    
+    </form>
+</body>
+</html>
